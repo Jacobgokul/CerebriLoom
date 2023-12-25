b3409163cfaa349a485593373f886368dcb65fef
Jacobgokul <gokulnathraguraman@gmail.com>
2023-12-25 14:47:11 +0530

    updated the version

--- utilities/ai_utils.py
+++ utilities/ai_utils.py
@@ -3,6 +3,11 @@ import streamlit as st
 
 openai.api_key = api_key = st.secrets["openai_key"]
 
+headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
 def mental_wellbeing(user_query):
     goal = """
     You are a mental wellbeing assistant. Your role is to provide comfort, mindfulness exercises, 
@@ -21,21 +26,18 @@ def mental_wellbeing(user_query):
         }
     ]
     
+    payload = {
+        "model": "gpt-4",
+        "messages": messages,
+    }
+
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=messages,
-        )
+        response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
         return response['choices'][0]['message']['content']
     except Exception as e:
         return f"Sorry, there was an error processing your request. Error: {e}"
     
 def QSync(question, image=None):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {api_key}"
-    }
-
     messages = [{
         "role": "user",
         "content": [{"type": "text", "text": question}]
